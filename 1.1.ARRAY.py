08c27f47d8e8fb408c9ed41ef223e792f20f93d4
sam <35342774+samueltomol@users.noreply.github.com>
2018-04-03 23:13:46 +0800

    Add files via upload

---
+++ 1.1.ARRAY.py
@@ -0,0 +1,31 @@
+#Samuel C. Tomol
+#github.com/samueltomol
+#ARRAY
+
+#creating array
+arr_1 = [10, 20, 30, 40, 50]
+print (arr_1)
+
+
+#adding an element
+add = ['a', 'b', 'c']
+add.append('d')
+print(add)
+
+#Accesing array using index
+arr_2 = [10, 20, 30, 40, 50]
+print(arr_2[0])
+print(arr_2[1])
+print(arr_2[2])
+print(arr_2[3])
+print(arr_2[4])
+
+
+#Exercise No.1
+arr_3 =[['S','h','e'], ['i','s'], ['b','e','a','u','t','i','f','u','l'], ['F','o','r'],[ 'm','e'], ['a','n','d',], ['i','m'], ['l','u','c','k','y']]
+arr_3.append(['t','h','a','n','k','s'])
+sam = [arr_3[1][0]," ",arr_3[8][0],arr_3[0][1],arr_3[2][2],arr_3[5][1],arr_3[7][3]," ",arr_3[7][4],arr_3[3][1], arr_3[2][3]]
+print (arr_3)
+print (sam)
+
+

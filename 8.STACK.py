ba33f7c4a85ac3e873eac123301d0133caf81a5c
sam <35342774+samueltomol@users.noreply.github.com>
2018-04-03 23:16:34 +0800

    Add files via upload

---
+++ 8.STACK.py
@@ -0,0 +1,37 @@
+#Samuel C. Tomol
+#github.com/samueltomol
+#STACKS
+
+class Stack:
+
+    def __init__(self):
+        self.stack = list()
+
+    def push(self,data):
+        if data not in self.stack:
+            self.stack.append(data)
+            return True
+        return False
+
+
+    def pop(self):
+        if len(self.stack)<=0:
+            return ("Stack Empty!")
+        return self.stack.pop()
+        
+    def size(self):
+        return len(self.stack)
+
+myStack = Stack()
+print(myStack.push(5))
+print(myStack.push(4))
+print(myStack.push(3))
+print(myStack.push(2))
+print(myStack.push(1))
+print(myStack.size())
+print(myStack.pop())
+print(myStack.pop())
+print(myStack.pop())
+print(myStack.pop())
+print(myStack.size())
+print(myStack.pop())
